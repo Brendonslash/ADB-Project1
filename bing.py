28bb9b7c7fb3e69614dbfb47e3a04bdacfc4e7cf
Parth Mehta <parth94@hotmail.com>
2016-10-10 17:16:51 -0400

    Final version with readme and transcript

--- bing.py
+++ bing.py
@@ -1,27 +1,15 @@
 import urllib2
 import base64
-#print "Enter your Web Query Human: "
 
-def execQuery(inp):
-	#To handle spaces in the query
-# 	if inp == "": 
-# 		print "Enter the query:"
-# 		inp = raw_input()
+def execQuery(inp,key):
+	#replace space by %20 in the query
 	inp = inp.replace(" ","%20")
-	#rint inp
 	bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27'+inp+'%27&$top=10&$format=json'
-	#print bingUrl
-	#Provide your account key here
-	accountKey = 'yaTtNJf/mq/VOXKXCliOjmUaeoL4hiK4akoPVAjvsdk'
-
+	accountKey = key
 	accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
 	headers = {'Authorization': 'Basic ' + accountKeyEnc}
 	req = urllib2.Request(bingUrl, headers = headers)
 	response = urllib2.urlopen(req)
-	content = response.read()
-	#content contains the xml/json response from Bing. 
-# 	print content
-	return content
-
-# inp = raw_input()
-# execQuery(inp)
+	#content contains the xml/json response from Bing.
+	content = response.read() 
+	return (bingUrl,content)
--- read_result.py
+++ read_result.py
@@ -3,269 +3,276 @@ import json
 import urllib
 import urllib2
 from bs4 import BeautifulSoup
-import re
-import lxml
-from lxml.html.clean import Cleaner
 import operator
 import string
 import requests
 import collections
-import wget
 import os
 import httplib2
 from bing import execQuery
-from nltk.stem.wordnet import WordNetLemmatizer
+from urllib2 import urlopen
+import requests
+import numbers
+import sys
 
-#gives frequency
+#This function takes the html source text and returns a Ordered dictionary of words i.e the term frequency for each page
 def word_count(string):
-	lemtzr = WordNetLemmatizer()
 	my_string = string.lower().split()
 	my_dict = {}
+	
+	#create a dict that contains each word in string as a key and the frequency of that word as its value
 	for item in my_string:
-		item = lemtzr.lemmatize(str(item))
 		if item in my_dict:
 			my_dict[item] += 1
 		else:
 			my_dict[item] = 1
+	
+	#remove stop-words	
 	f1=open('stop.txt','r')
 	stopFile=f1.read()
 	for word in stopFile.split():
 		if  word in my_dict:
 			del my_dict[word]
+			
+	#remove words that are already present in the query
 	for word in my_query.split():
 		if  word in my_dict:
 			del my_dict[word]
+			
+	#convert into a sorted dict and return it
 	sorted_dict = collections.OrderedDict(sorted(my_dict.items(), key=operator.itemgetter(1)))
 	return sorted_dict
-	
-#gives minimum position from word
-def minDist(lis,pos):
-	return max([1.0/abs(pos-x) for x in lis])
-	
-#gives proximity for all words
-def wordPositionWeights(sentence,reference):
-	sentence = sentence.lower()
-	distMap={}
-	referencePos = []
-	count=0
-	f1=open('stop.txt','r')
-	stopFile=f1.read()
-	table = string.maketrans("","")
-	sentence=sentence.translate(table, string.punctuation)
-	for word in sentence.split():
-		count+=1
-		if(word==reference):
-			referencePos.append(count)
-			pos = 0
-	for word in sentence.split():
-		pos+=1
-		if(word==reference):
-			continue
-		if(word in distMap.keys() and distMap[word]>minDist(referencePos,pos)):
-			distMap[word] = minDist(referencePos,pos)
-		elif not( word in distMap.keys()):
-			distMap[word] = minDist(referencePos,pos)
-	for word in distMap.keys():
-		if word in stopFile.split():
-			del distMap[word]
-	return distMap
-    
-#print wordPositionWeights(" Milky - definition of milky by The Free Dictionary","milky")
+
+#Takes list of relevant documents and returns a list which contains dicts of term frequencies for each 'relevant' web page
 def get_from_docs(relevant):
 	tf_list=[]
-	#print relevant
-	#print len(relevant)
+
+	#Getting only text-web pages 
 	for result in relevant:
 		tf_dict={}
 		url=result['Url']
-# 		print url
-# 		url=getContentLocation(url)
-# 		print "Redirected:",
-# 		print url
-		#im=raw_input()
-		#print result['Url']
-		#page = requests.get(url)
-		#htmlSource = page.text
-		
-		#browser = webdriver.Firefox()
-		#browser.get(url)
-		filename = wget.download(url)
-		htmlSource = open(filename,'r').read()
-		os.remove(filename)
-		#print htmlSource
-		soup=BeautifulSoup(htmlSource,'html.parser')
+		opener = urllib2.build_opener()
+		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+		try:
+			response = opener.open(url)
+			info = response.info()
+			if 'text' not in (info.dict['content-type']):      
+				continue
+		except:
+			continue
+			
+		#Taking text only from selected HTML tags
+		infile = opener.open(url)
+		page = infile.read()
+		soup=BeautifulSoup(page,'html.parser')
 		for script in soup(["script", "style"]):
 	    		script.extract()
 		paras=soup.find_all(['p','title','b','i','h1','h2','h3','h4','h5','h6'])
 		text=''
 		for i in xrange(0,len(paras)):
 			text=text + paras[i].get_text().encode('utf-8')
+		
 		#Handling unicode characters	
 		lines = (line.strip() for line in text.splitlines())
 		chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 		text = '\n'.join(chunk for chunk in chunks if chunk)
-		text=text.decode('unicode_escape').encode('ascii','ignore')
+		try:
+			text=text.decode('unicode_escape').encode('ascii','ignore')
+		except:
+			continue
 		#Removing punctuations
 		table = string.maketrans("","")
 		text=text.translate(table, string.punctuation)
+		
+		#call word_count function to return an ordered dict of words for this webpage, 
+		#and append it to the list of dicts for each relevant page.
 		tf_dict=word_count(text)
-		#print text
-		#print tf_dict
-		#inp = raw_input()
-		# if 'way' in tf_dict.keys():
-# 			print tf_dict['way']
 		tf_list.append(tf_dict)
-		#Take words from title
-		#title_text=result['Title']
-		#title_text=title_text.decode('unicode_escape').encode('ascii','ignore')
-		#print "Title Count:"
-		#title_tf_dict=word_count(title_text)
 	return tf_list
 
+#flag is a boolean to exit the loop
 flag=True
-print "Input Query:"
-my_query=raw_input().lower()
-print "Input required precision between 0 and 1:"
-precision_threshold=float(raw_input())
-while(flag):	
-	json_string=execQuery(my_query)
-	# f = open('parseData5','r')
-	# json_string =f.read()
+args = sys.argv
+authKey = ''
+precision_threshold = 0
+my_query = ''
+#Handle incorrect calls
+try:
+	authKey  = args[1]
+	precision_threshold = float(args[2])
+	my_query = args[3]
+except:
+	"Human the usage is python read_result.py key precision@k query"
+	pass
+
+#t_word is a list of words in titles and descriptions of all relevant pages, that have first letter capitalised.
+t_word=[]
+Round=1
+while(flag):
+	print "Round "+ str(Round)
+	print "Parameters: " 
+	print "Client Key = " + authKey
+	print "Query = " + my_query
+	print "Precision = " + str(precision_threshold)
+	try:
+		alpha=execQuery(my_query,authKey)
+		print "URL: " + alpha[0]
+	except:
+		print "Authentication Error. Is your key correct?"
+	
+	#alpha[1] contains the content returned by execQuery in bing.py
+	json_string = alpha[1]
 	my_map = json.loads(json_string)
 	results = my_map['d']['results']
 	relevant = []
 	irrelevant = []
 	precision=0
-
-	for result in results:
-		print "URL: " + result['Url'].encode('utf-8')
-		print "Title: " + result['Title'].encode('utf-8')
-		print "Description: " + result['Description'].encode('utf-8')
-		print "Please give your feeedback[y|n]"
-		#print wordPositionWeights(result['Title'].encode('utf-8'),'musk')
-		my_inp = raw_input()
-		#print tf_dict
-		while(my_inp!='y' and my_inp!='n'):
-			print "Human please enter valid feedback"
+	print "Total Number of results : "+str(len(results))
+	
+	#Handle the case that bing returns <10 results
+	if len(results) < 10:
+		print "Program Terminated. Results returned were less than 10"
+		break
+	else:
+		print "Bing Search Results:"
+		print "===================="
+		counter = 1
+		for result in results:
+			print "Result "+ str(counter)
+			print "["
+			print "  URL: " + result['Url'].encode('utf-8')
+			print "  Title: " + result['Title'].encode('utf-8')
+			print "  Description: " + result['Description'].encode('utf-8')
+			print "]"
+			print "Relevant (Y/N)?"
 			my_inp = raw_input()
-		if my_inp.lower()=='y':
-			precision+=1
-			relevant.append(result)
-		elif my_inp.lower()=='n':
-			irrelevant.append(result)
-	tf_list = get_from_docs(relevant)
-	#sprint tf_list
-	scores=[]
-	#print tf_list
-	all_keys = []
-	for doc in tf_list:
-		all_keys+=doc.keys()
-	#print all_keys
-	term_frequency = dict.fromkeys(list(set(all_keys)))
-	for key in all_keys:
-		term_frequency[key]=0
-	
-	for doc in tf_list:
+			while(my_inp!='y' and my_inp!='n'):
+				print "Human please enter valid feedback"
+				my_inp = raw_input()
+			if my_inp.lower()=='y':
+				precision+=1
+				relevant.append(result)
+			elif my_inp.lower()=='n':
+				irrelevant.append(result)
+			counter+=1
+		#function get_from_docs returns a list of dicts of term frequencies all 'relevant' web pages
+		tf_list = get_from_docs(relevant)
+		
+		#Combine the list of dicts into one single dict, adding up values of same key values.
+		scores=[]
+		all_keys = []
+		for doc in tf_list:
+			all_keys+=doc.keys()
+		term_frequency = dict.fromkeys(list(set(all_keys)))
 		for key in all_keys:
-			if(key in doc.keys()):
-				term_frequency[key]+=doc[key]
-	# for doc in xrange(len(tf_list)):
-	# 	for key in tf_list[doc]:
-	# 		for d in xrange(len(tf_list)):
-	# 			if key in tf_list[d]:
-	# 				if not (key in term_frequency):term_frequency[key]=0
-	# 				term_frequency[key]=term_frequency[key]+tf_list[d][key]
-	# 			else:
-	# 				if not (key in term_frequency):term_frequency[key]=0
-	# 				term_frequency[key]=tf_list[doc][key]
+			term_frequency[key]=0
+		
+		for doc in tf_list:
+			for key in all_keys:
+				if(key in doc.keys()):
+					term_frequency[key]+=doc[key]
 
-	term_frequency=collections.OrderedDict(sorted(term_frequency.items(), key=operator.itemgetter(1)))
-	title_string=''
-	desc_string=''
-	for result in relevant:
-		title_string+=result['Title']
-		desc_string+=result['Description']
-	
+		term_frequency=collections.OrderedDict(sorted(term_frequency.items(), key=operator.itemgetter(1)))
+		title_string=''
+		desc_string=''
+		for result in relevant:
+			title_string+=result['Title']
+			desc_string+=result['Description']
+		
+
+		td_terms_list=[]
+		title_append1=''
+		title_append2=''
+		td_word_frequency=collections.OrderedDict()
+		if(len(term_frequency)!=0):
+			if term_frequency.items()[len(term_frequency)-1][1] < 7:
+				table = string.maketrans("","")
+				#handle unicode characters
+				td_lines=(title_string.encode('utf-8') + " " + desc_string.encode('utf-8')).lower()
+				#remove punctuations
+				td_lines=td_lines.translate(table, string.punctuation)
+				td_words=td_lines.split()
+				f3=open('stop.txt','r')
+				stopFile=f3.read()
+				#remove stop-words
+				td_words = list(filter(lambda x: x not in stopFile.split(),td_words))
+				for word in td_words:
+					if not(word.isdigit()):
+						td_terms_list.append(word)
+				td_string=' '.join(td_terms_list)
+				td_word_frequency=word_count(td_string)
+				title_append1=td_word_frequency.items()[len(td_word_frequency)-1][0]
+				title_append2=td_word_frequency.items()[len(td_word_frequency)-2][0]
+		
+		else: 
+			print "Program terminated."
+			break	
+
 	max=0
-	title_word=''	
-	#print title_string.encode('utf-8') + desc_string.encode('utf-8')
+	title_word=''
+	f=open('stop.txt','r')	
+	stopFile=f.read()
 	for word in title_string.encode('utf-8').split() + desc_string.encode('utf-8').split():
-		if word in term_frequency.keys() and not(word.isdigit()) and term_frequency[word] > max:
-			#print word
+		#Remove punctuation
+		table = string.maketrans("","")
+		word = word.translate(table, string.punctuation)
+		#word should not be a digit, first letter should be capital, and should not occur in the query or stop-words
+		if word!="" and word[0].isupper() and word.lower() not in my_query.lower() and word.lower() not in stopFile.split():
+			t_word.append(word)
+		if word.decode('utf-8') in term_frequency.keys() and not(word.isdigit()) and term_frequency[word] > max:
 			max = term_frequency[word]
 			title_word=word
-
-	td_terms_list=[]
-	title_append1=''
-	title_append2=''
-	#print term_frequency.items()[len(term_frequency)-1][1]
-	if(len(term_frequency)!=0):
-		if term_frequency.items()[len(term_frequency)-1][1] < 7:
-			table = string.maketrans("","")
-		#print title_word
-		
-			td_lines=(title_string.encode('utf-8') + " " + desc_string.encode('utf-8')).lower()
-			print td_lines
-			td_lines=td_lines.translate(table, string.punctuation)
 	
-			td_words=td_lines.split()
-			#print td_words
-			f3=open('stop.txt','r')
-			stopFile=f3.read()
-			td_words = list(filter(lambda x: x not in stopFile.split(),td_words))
-			for word in td_words:
-				if not(word.isdigit()):
-					td_terms_list.append(word)
-			td_string=' '.join(td_terms_list)
-			td_word_frequency=word_count(td_string)
-			print td_word_frequency
-			title_append1=td_word_frequency.items()[len(td_word_frequency)-1][0]
-			title_append2=td_word_frequency.items()[len(td_word_frequency)-2][0]
-	
-	else: 
-		print "No Results Returned by Bing!"
-		break	
-	#print td_terms_list
-	#print title_append1
+	#find most frequent word with first letter capitalised in the titles and descriptions of all of the relevant documents
+	most_frequent_count = 0
+	most_frequent_word = t_word[0]
+	for caps in t_word:
+		if (caps.lower() in term_frequency.keys()) and (term_frequency[caps.lower()] > most_frequent_count):
+			most_frequent_word = caps
+			most_frequent_count = term_frequency[caps.lower()]
 	i=1
 	while(i<len(term_frequency)):
 		append1=term_frequency.items()[len(term_frequency)-i][0]
 		i+=1
-		if not(append1.isdigit()) and (append1 != title_word) and (append1 not in my_query):
+		#word should not be a digit,  should not occur in the query and should not be the same as the one taken from title/description
+		if not(append1.isdigit()) and (append1 != title_word) and (append1 not in my_query) and (append1 != most_frequent_word.lower()):
 			break
-		
+	i=1	
 	while(i<len(term_frequency)):
 		append2=term_frequency.items()[len(term_frequency)-i-1][0]
 		i+=1
-		if not(append2.isdigit()) and (append2 != title_word) and (append2 not in my_query):
-			break
-				
-			
-			
-	#print append1, append2
+		#word should not be a digit,  should not occur in the query and should not be the same as the one taken from title/description
+		if not(append2.isdigit()) and (append2 != title_word) and (append2 not in my_query) and (append2 != most_frequent_word.lower()):
+			break		
+	print "==========================================================="
+	aug = []
+	my_query_old = my_query
 	if(len(term_frequency)!=0):
 		if term_frequency.items()[len(term_frequency)-1][1] >= 10:
-			my_query = my_query + " "+ append1 + " " + append2 #+ " " + title_word
+			aug.append(append1)
+			aug.append(most_frequent_word.lower())
+			my_query = my_query + " "+ append1 + " " + most_frequent_word.lower()
+		#If not enough content from HTML of the pages, use only titles and descriptions
 		if term_frequency.items()[len(term_frequency)-1][1] < 10:
+			aug.append(title_append1)
+			aug.append(title_append2)
 			my_query=my_query + " " + title_append1 + " " + title_append2
 	
 	else:
 		print "No results returned"
 		break
-	
-	#print term_frequency
-	print "New Query is:",
-	print my_query
 	precision=precision*1.0/10
-	print precision
-	if precision >= precision_threshold: flag=False
-	#print relevant
-	#print my_map['d']['results'][0]
+	print "FEEDBACK SUMMARY"
+	print "Query " + my_query_old
+	print "Precision " + str(precision)
+	Round+=1
+	if precision >= precision_threshold: 
+		flag=False
+		print "Desired Precision reached, done"
+	else:
+		print "Still below desired precision of "+ str(precision_threshold)
+		print "Augmenting by " + aug[0] +" "+ aug[1]
 	
-	#TODO: Catch errors, terminate on 0, clean code, refactor, testing
-	#TODO: take from title only in second iteration
-	#TODO: order is important
-	#TODO: Sergey Brin
-	#TODO: Noun with capital letter in first iteration
+	
 
 
